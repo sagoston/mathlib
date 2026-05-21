48dc9dc9857c88a97fcf83a90c279dbac132224e
Agoston <Steve.Agoston@sony.com>
2026-05-21 19:47:32 +0100

    test: intentionally break add for downstream failure test

--- src/mathlib.cpp
+++ src/mathlib.cpp
@@ -2,7 +2,7 @@
 
 namespace mathlib {
     int add(int a, int b) {
-        return a + b;
+        return a - b;  // intentional break for failure test
     }
 
     std::string greet(const std::string& name) {
